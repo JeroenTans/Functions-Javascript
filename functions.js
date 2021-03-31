d4c9a55c8f755b7d0bb2381dab3eac1ceb075019
JeroenTans <jeroentans1@gmail.com>
2021-03-31 11:29:45 +0200

    message

--- functions.js
+++ functions.js
@@ -3,16 +3,32 @@
 // ---- Verwachte uitkomsten:
 // 3, 4, 5 geeft 12
 // 11, 3, 9 geeft 23
+const add = (number1, number2, number3) =>  {
+    return number1 + number2 + number3;
+}
+console.log(add(3, 4, 5));
+console.log(add(11, 3, 9));
 
-
+function addTwo (number1, number2, number3) {
+    return number1 + number2 + number3;
+}
+console.log(addTwo(3, 4, 5));
+console.log(addTwo(11, 3, 9));
 
 /* Opdracht 2 */
 // Schrijf een functie die twee strings verwacht en deze aan elkaar geplakt teruggeeft.
 // Je mag hier geen String Object methoden voor gebruiken.
 // ---- Verwachte uitkomsten:
 // "abra", "cadabra" geeft "abracadabra"
+const stickTogether = (firstWord, secondWord) => {
+    return firstWord + secondWord;
+}
+console.log(stickTogether('abra', 'cadabra'));
 
-
+function stickTogetherTwo (firstWord, secondWord) {
+    return firstWord + secondWord;
+}
+console.log(stickTogetherTwo('abra', 'cadabra'));
 
 /* Opdracht 3 */
 // Schrijf een functie die twee parameters verwacht: een woord en een karakter
@@ -21,8 +37,19 @@
 // "bril", "*" geeft "*bril*"
 // "beep", "_" geeft "_beep_"
 // "kaas", "Q" geeft "QkaasQ"
+const wrapAround = (firstSymbol, character) => {
+    return firstSymbol + character + firstSymbol;
+}
+console.log(wrapAround('*', 'bril'));
+console.log(wrapAround('_', 'beep'));
+console.log(wrapAround('Q', 'kaas'));
 
-
+function wrapAroundTwo (firstSymbol, character) {
+    return firstSymbol + character + firstSymbol;
+}
+console.log(wrapAroundTwo('*', 'bril'));
+console.log(wrapAroundTwo('_', 'beep'));
+console.log(wrapAroundTwo('Q', 'kaas'));
 
 /* Opdracht 4 */
 // Schrijf een functie die een array van strings verwacht. De functie geeft
@@ -31,7 +58,19 @@
 // ---- Verwachte uitkomsten:
 // ["abra", "cadabra"] geeft "abracadabra"
 // ["a", "b", "c"] geeft "abc"
+function writtenTogether (firstWord1, secondWord2, firstLetter, secondLetter, thirdLetter) {
+    const allWords1 = firstWord1 + secondWord2;
+    const allLetters = firstLetter + secondLetter + thirdLetter;
+    return [allWords1, allLetters];
+}
+console.log(writtenTogether('abra', 'cadabra', 'a', 'b', 'c'));
 
+function writtenTogetherTwo (firstArray, secondArray) {
+    let allWords2 = ['abra', 'cadabra']
+    return allWords2;
+}
+
+console.log(writtenTogetherTwo.length);
 
 
 /* Opdracht 5 */
